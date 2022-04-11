e300598d5b36fea4ea914d72302dea069dbaaa46
Kursat Dogan <kursatdogan27900@gmail.com>
2022-04-11 10:04:28 +0300

    get response and dinamic id have been added

--- src/test/java/com/weavr/API/goRestTest.java
+++ src/test/java/com/weavr/API/goRestTest.java
@@ -5,14 +5,12 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.*;
-//import static org.testng.Assert.assertEquals;
+
 
 public class goRestTest {
 
@@ -24,108 +22,95 @@ public class goRestTest {
         RestAssured.baseURI="https://gorest.co.in/public/v2";
     }
 
-
-
     @Test
     public void getRequest(){
-        //get request from member who already exist
-        Response response = given().accept(ContentType.JSON)
-                .and().pathParam("id",4064)
-                .when().get("/users/{id}");
-        assertEquals(response.statusCode(),200);
-        assertEquals(response.contentType(),"application/json; charset=utf-8");
 
-        int id = response.path("id");
-        String name = response.path("name");
-        String email = response.path("email");
-        String gender = response.path("gender");
-        String status = response.path("status");
+    //create a new user with post request and using Map
 
-        assertEquals(4064,id);
-        assertEquals("Triloki Iyengar",name);
-        assertEquals("iyengar_triloki@pfannerstill.com",email);
-        assertEquals(gender,"male");
-        assertEquals(status,"inactive");
-
-    }
-
-    @Test
-    public void postRequest(){
-        //create a new user with post request and using Map
-        Map<String,Object> postMap = new HashMap<>();
-        postMap.put("name","Michael Jordan");
-        postMap.put("email","Mjordan@nba.com");
-        postMap.put("gender","male");
-        postMap.put("status","inactive");
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","Michael Jordan");
+        map.put("email","Mjordan@nba.com");
+        map.put("gender","male");
+        map.put("status","inactive");
         Response postResponse = given().accept(ContentType.JSON)
                 .and().contentType(ContentType.JSON)
                 .and().auth().oauth2("6397d92422bc792818606ac610306220d8c14bb27271bbf4c54e0a5968507737")
-                .body(postMap)
+                .body(map)
                 .when().post("/users");
         postResponse.prettyPrint();
         assertEquals(201,postResponse.statusCode());
         assertEquals("application/json; charset=utf-8",postResponse.contentType());
-        assertEquals("Michael Jordan",postMap.get("name"));
-        assertEquals("Mjordan@nba.com",postMap.get("email"));
-        assertEquals("male",postMap.get("gender"));
-        assertEquals("inactive",postMap.get("status"));
+        assertEquals("Michael Jordan",map.get("name"));
+        assertEquals("Mjordan@nba.com",map.get("email"));
+        assertEquals("male",map.get("gender"));
+        assertEquals("inactive",map.get("status"));
         newId=postResponse.path("id");
-    }
+        System.out.println("the user whom id:" + newId + " has been created with POST request");
+        System.out.println("And the the response body has been checked by using MAP");
 
-    @Test
-    public void putRequest(){
-        //Updating the user who has been already created by post request with using map
-        Map<String,Object> postMap = new HashMap<>();
-        postMap.put("name","Michael Jordan");
-        postMap.put("email","micjordan@nba.com");
-        postMap.put("status","active");
+    //With get response the payload is checked and the path method is used for getting each info
+        Response getResponse = given().accept(ContentType.JSON)
+                .auth().oauth2("6397d92422bc792818606ac610306220d8c14bb27271bbf4c54e0a5968507737")
+                        .pathParam("id",newId)
+                                .when().get("/users/{id}");
+        assertEquals(200,getResponse.statusCode());
+        assertEquals("application/json; charset=utf-8",getResponse.contentType());
+        assertEquals("Michael Jordan",getResponse.path("name"));
+        assertEquals("Mjordan@nba.com",getResponse.path("email"));
+        assertEquals("male",getResponse.path("gender"));
+        assertEquals("inactive",getResponse.path("status"));
+        System.out.println("The payload is checked by using GET response and path method");
+
+    //Updating the user who has been already created by post request with using map;
+        map.put("name","Michael Jordan");
+        map.put("email","mikejordan@nba.com");
+        map.put("status","active");
         Response putResponse = given().accept(ContentType.JSON)
                 .and().contentType(ContentType.JSON)
-                .and().pathParam("id",4604)
+                .and().pathParam("id",newId)
                 .and().auth().oauth2("6397d92422bc792818606ac610306220d8c14bb27271bbf4c54e0a5968507737")
-                .body(postMap)
+                .body(map)
                 .when().put("/users/{id}");
         putResponse.prettyPrint();
         assertEquals(200,putResponse.statusCode());
         assertEquals("application/json; charset=utf-8",putResponse.contentType());
-        assertEquals("Michael Jordan",postMap.get("name"));
-        assertEquals("micjordan@nba.com",postMap.get("email"));
-        assertEquals("active",postMap.get("status"));
+        assertEquals("Michael Jordan",map.get("name"));
+        assertEquals("mikejordan@nba.com",map.get("email"));
+        assertEquals("active",map.get("status"));
         assertEquals("male",putResponse.path("gender"));
+        System.out.println("the user who already exist has been updating by using PUT request");
 
-    }
 
-    @Test
-    public void patchRequest(){
-        //updating the user name and email info with patch method using Map
-        //for assertion JsonPath object has been used
-        Map<String,Object> patchMap = new HashMap<>();
-        patchMap.put("name","Michael Jordan2");
-        patchMap.put("email","mcjordan2@nba.com");
-        Response response = given().accept(ContentType.JSON)
+    //updating the user name and email info with patch method using Map
+    //for assertion JsonPath object has been used
+
+
+        map.put("email","mcjordan2@nba.com");
+        Response patchResponse = given().accept(ContentType.JSON)
                 .and().contentType(ContentType.JSON)
-                .and().pathParam("id",4604)
+                .and().pathParam("id",newId)
                 .auth().oauth2("6397d92422bc792818606ac610306220d8c14bb27271bbf4c54e0a5968507737")
-                .body(patchMap)
+                .body(map)
                 .when().patch("/users/{id}");
-        response.prettyPrint();
-        assertEquals(response.statusCode(),200);
-        assertEquals("application/json; charset=utf-8",response.contentType());
+        patchResponse.prettyPrint();
+        assertEquals(patchResponse.statusCode(),200);
+        assertEquals("application/json; charset=utf-8",patchResponse.contentType());
 
-        JsonPath json = response.jsonPath();
+        JsonPath json = patchResponse.jsonPath();
 
-        assertEquals("Michael Jordan2",json.getString("name"));
+        assertEquals("Michael Jordan",json.getString("name"));
         assertEquals("mcjordan2@nba.com",json.getString("email"));
+        System.out.println("Only the user email info has been updating by usin PATCH request");
 
-    }
 
-    @Test
-    public void deleteRequest(){
-        //delete the user who created by post request
-        Response response = given().accept(ContentType.JSON)
+    //delete the user who created by post request
+        Response deleteResponse = given().accept(ContentType.JSON)
                 .auth().oauth2("6397d92422bc792818606ac610306220d8c14bb27271bbf4c54e0a5968507737")
-                .when().delete("/users/4604");
-        assertEquals(204,response.statusCode());
+                .and().pathParam("id",newId )
+                .when().delete("/users/{id}");
+        assertEquals(204,deleteResponse.statusCode());
+        System.out.println("********* After DELETE request *********");
+        System.out.println("The user whom id:" + newId + " has been deleted");
 
     }
 
